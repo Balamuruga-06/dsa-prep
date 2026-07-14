2fbcad4a5a7fdd7264ea1d0d42db1d71cf6417b8
Balamuruga-06 <68864586+Balamuruga-06@users.noreply.github.com>
2026-07-14 08:25:09 +0530

    Time: 2 ms (92.18%), Space: 47.7 MB (57.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int writeIndex=0;
-        int total = nums.length;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                nums[writeIndex] = nums[i];
+                int temp = nums[i];
+                nums[i] = nums[writeIndex];
+                nums[writeIndex]=temp;
                 writeIndex++;
             }
         }
-        while(writeIndex > 0 && writeIndex<nums.length){
-            nums[writeIndex] = 0;
-            writeIndex++;
-        }
+        
         
 
     }
